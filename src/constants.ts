ba868946e467dc62c117d6da88aa0ec34d35fcf5
NINJATSS <143434164+NINJATSS@users.noreply.github.com>
2024-02-16 10:47:34 +0000

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -20,12 +20,13 @@ export const TOKENS: TokenMeta[] = [
     image: "https://assets.coingecko.com/coins/images/34188/standard/jup.png?1704266489",
     decimals: 6,
     baseWager: 1 * 1e6,
-  }
-,
-{
-    mint: new PublicKey("vSYAPufpX2a4HpnkXguuoTznA5r2yZan8yP2RGnkvKm"),
-    symbol: 'PNDC',
-    name: 'PNDC',
-    image: "https://media.discordapp.net/attachments/1127551018297741432/1207996226738589766/20240216_102515.jpg?ex=65e1ad27&is=65cf3827&hm=60f3ca8584451f262394ae06ba1ebde496ddec3d5ec94c8752d779050a19f3db&",
-    decimals: 5,
-    baseWager: 70000000000 * 1e5,
+  },
+  {
+      mint: new PublicKey("vSYAPufpX2a4HpnkXguuoTznA5r2yZan8yP2RGnkvKm"),
+      symbol: 'PNDC',
+      name: 'PNDC',
+      image: "https://media.discordapp.net/attachments/1127551018297741432/1207996226738589766/20240216_102515.jpg?ex=65e1ad27&is=65cf3827&hm=60f3ca8584451f262394ae06ba1ebde496ddec3d5ec94c8752d779050a19f3db&",
+      decimals: 5,
+      baseWager: 70000000000 * 1e5,
+    },
+]
